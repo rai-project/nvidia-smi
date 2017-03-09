95159033f43aa02e0a25a448215fad2e8cfd4c2e
Abdul Dakkak <abduld@wolfram.com>
2017-03-09 20:34:06 +0000

    start working on cuda docker plugin

---
+++ init.go
@@ -0,0 +1,18 @@
+package nvidiasmi
+
+import (
+	"github.com/Sirupsen/logrus"
+
+	"github.com/rai-project/config"
+	logger "github.com/rai-project/logger"
+)
+
+var (
+	log *logrus.Entry
+)
+
+func init() {
+	config.OnInit(func() {
+		log = logger.New().WithField("pkg", "nvidia-smi")
+	})
+}
--- nvidiasmi.go
+++ nvidiasmi.go
@@ -6,10 +6,15 @@ import (
 	"encoding/xml"
 
 	"github.com/pkg/errors"
+	"github.com/rai-project/config"
 )
 
 const (
 	BinaryName = "nvidia-smi"
+)
+
+var (
+	Info *NvidiaSmi
 )
 
 func New() (*NvidiaSmi, error) {
@@ -28,4 +33,15 @@ func New() (*NvidiaSmi, error) {
 		return nil, errors.Wrapf(err, "failed to unmarshal %v -x -q -a", exe)
 	}
 	return res, nil
+}
+
+func init() {
+	config.AfterInit(func() {
+		info, err := New()
+		if err != nil {
+			log.WithError(err).Error("was not able to get nvidia-smi info")
+			return
+		}
+		Info = info
+	})
 }
