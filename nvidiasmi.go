aa732cba5c60f48fa057aa2dbbe07fc5e1434bd6
Abdul Dakkak <abduld@wolfram.com>
2017-03-09 19:44:17 +0000

    add run cmd

--- nvidiasmi.go
+++ nvidiasmi.go
@@ -1,1 +1,31 @@
 package nvidiasmi
+
+import (
+	"os/exec"
+
+	"encoding/xml"
+
+	"github.com/pkg/errors"
+)
+
+const (
+	BinaryName = "nvidia-smi"
+)
+
+func New() (*NvidiaSmi, error) {
+	exe, err := exec.LookPath(BinaryName)
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot find %v in path", BinaryName)
+	}
+	cmd := exec.Command(exe, "-x", "-q", "-a")
+	bts, err := cmd.CombinedOutput()
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to run %v -x -q -a", exe)
+	}
+	res := new(NvidiaSmi)
+	err = xml.Unmarshal(bts, res)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to unmarshal %v -x -q -a", exe)
+	}
+	return res, nil
+}
---
+++ nvidiasmi_test.go
@@ -0,0 +1,29 @@
+package nvidiasmi
+
+import (
+	"encoding/xml"
+	"testing"
+
+	rice "github.com/GeertJohan/go.rice"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	box         = rice.MustFindBox("_fixtures")
+	example1XML = box.MustBytes("example1.xml")
+	example2XML = box.MustBytes("example2.xml")
+)
+
+func TestUnmarshal(t *testing.T) {
+	var info NvidiaSmi
+	err := xml.Unmarshal(example1XML, &info)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, info)
+}
+
+func TestNew(t *testing.T) {
+	info, err := New()
+	if err != nil {
+		assert.NotNil(t, info)
+	}
+}
