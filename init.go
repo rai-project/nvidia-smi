54205e2a852e9054e8d364627ed7f1d5b8d31c05
Abdul Dakkak <abduld@wolfram.com>
2017-03-18 08:54:57 +0000

    update config handeling

--- init.go
+++ init.go
@@ -12,5 +12,15 @@ var (
 func init() {
 	config.AfterInit(func() {
 		log = logger.New().WithField("pkg", "nvidia-smi")
+
+		HasGPU = false
+		info, err := New()
+		if err != nil {
+			log.WithError(err).Error("was not able to get nvidia-smi info")
+			return
+		}
+		Info = info
+		GPUCount = len(Info.GPUS)
+		HasGPU = GPUCount > 0
 	})
 }
--- nvidiasmi.go
+++ nvidiasmi.go
@@ -36,17 +36,3 @@ func New() (*NvidiaSmi, error) {
 	}
 	return res, nil
 }
-
-func init() {
-	config.AfterInit(func() {
-		HasGPU = false
-		info, err := New()
-		if err != nil {
-			log.WithError(err).Error("was not able to get nvidia-smi info")
-			return
-		}
-		Info = info
-		GPUCount = len(Info.GPUS)
-		HasGPU = GPUCount > 0
-	})
-}
