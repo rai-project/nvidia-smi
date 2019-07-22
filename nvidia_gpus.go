219c09ab27c3e2bfa17b9f496f17e69de09100d0
Abdul Dakkak <dakkak@illinois.edu>
2019-07-22 23:48:06 +0000

    add more gpu infos

--- nvidia_gpus.go
+++ nvidia_gpus.go
@@ -12,6 +12,17 @@ type NvidiaGPU struct {
 }
 
 var (
+	NVIDIAT4 = NvidiaGPU{
+		Name:              "Tesla T4",
+		NumSMs:            40,
+		ComputeCapability: 7.5,
+		Architecture:      "Volta",
+		Interconnect:      PCIe3{},
+		ClockRate:         int64(1530),
+		PeekGFlops:        int64(8100),
+		MemoryBandwidth:   float64(300),
+	}
+
 	NVIDIATeslaV100SXM2 = NvidiaGPU{
 		Name:              "Tesla V100-SXM2-16GB",
 		NumSMs:            80,
@@ -32,9 +43,33 @@ var (
 		MemoryBandwidth: float64(900),
 	}
 
+	// https://devblogs.nvidia.com/new-pascal-gpus-accelerate-inference-in-the-data-center/
+	NVIDIATeslaP4 = NvidiaGPU{
+		Name:              "Tesla P4",
+		NumSMs:            20,
+		ComputeCapability: 6.1,
+		Architecture:      "Pascal",
+		Interconnect:      PCIe3{},
+		ClockRate:         int64(1063),
+		PeekGFlops:        int64(5442),
+		MemoryBandwidth:   float64(192),
+	}
+
+	NVIDIATeslaP40 = NvidiaGPU{
+		Name:              "Tesla P40",
+		NumSMs:            30,
+		ComputeCapability: 6.1,
+		Architecture:      "Pascal",
+		Interconnect:      PCIe3{},
+		ClockRate:         int64(1531),
+		PeekGFlops:        int64(11758),
+		MemoryBandwidth:   float64(346),
+	}
+
 	NVIDIATeslaP100SXM2 = NvidiaGPU{
 		Name:            "TESLA P100 SXM2",
 		Architecture:    "Pascal",
+		NumSMs:          56,
 		Interconnect:    SXM2{},
 		ClockRate:       int64(1481),
 		PeekGFlops:      int64(10600),
@@ -44,6 +79,7 @@ var (
 	NVIDIATeslaP100PCIe = NvidiaGPU{
 		Name:            "TESLA P100 PCIE",
 		Architecture:    "Pascal",
+		NumSMs:          56,
 		Interconnect:    PCIe2{},
 		ClockRate:       int64(1328),
 		PeekGFlops:      int64(9300),
@@ -92,10 +128,12 @@ var (
 		PeekGFlops:      int64(4290),
 		MemoryBandwidth: float64(288),
 	}
+
 	NVIDIAK80 = NvidiaGPU{
 		Name:            "K80",
 		Architecture:    "Kepler",
 		Interconnect:    PCIe2{},
+		NumSMs:          13,
 		ClockRate:       int64(560),
 		PeekGFlops:      int64(5600),
 		MemoryBandwidth: float64(480),
@@ -104,6 +142,8 @@ var (
 	NvidiaGPUs = []NvidiaGPU{
 		NVIDIATeslaV100SXM2,
 		NVIDIATeslaV100PCIe,
+		NVIDIATeslaP4,
+		NVIDIATeslaP40,
 		NVIDIATeslaP100SXM2,
 		NVIDIATeslaP100PCIe,
 		NVIDIATitanXP,
